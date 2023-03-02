fdeb7c4fbc1d08cf7464e3077ffcd9f266fae7b5
tolik vasylov <tolik.vasilov174@gmail.com>
2023-03-02 18:22:24 +0200

    Добавьте файлы проекта.

---
+++ ConsoleApplication3/ConsoleApplication3.cpp
@@ -0,0 +1,22 @@
+﻿
+
+#include <iostream>
+#include <Windows.h>
+#include <string>
+
+using namespace std;
+int main()
+{
+	SetConsoleOutputCP(1251);
+	SetConsoleCP(1251);
+	cout << "hi i am tolik\n";
+	string string1, string2;
+	string1 = "hi i am tolik ";
+	string2 = string1.substr(3, 11);
+	cout << string2;
+
+
+
+
+
+}
